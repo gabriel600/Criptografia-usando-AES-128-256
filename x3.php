ae965a6e23721a5c8b00c1015b5f8fc8cac2dece
Gabriel <75588510+gabriel600@users.noreply.github.com>
2025-07-21 21:51:36 -0300

    Update x3.php

--- x3.php
+++ x3.php
@@ -1,5 +1,5 @@
 ﻿<?php
-// Define a chave de criptografia gerada no servidor
+// Define a sua chave de criptografia
 $valor = "0F 0D 00 00 00 99 88 06 01 07 44 00 00 83 49 56 67 00 43 22 02 65 91 00 04 54 60 03"; // adcione sua chave privada aqui!
 
 // Codifica a chave em base64 para torná-la "oculta" e não legível facilmente
